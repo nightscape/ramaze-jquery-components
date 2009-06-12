1babe732574716732ae85039487a55ea27a49ead
Martin.Mauch <martin.mauch@gmail.com>
2009-06-12 16:15:06 +0200

    Removed unnecessary show method in LiveSearchController\nAdded :jquery as app name to map call

--- lib/controller/live_search.rb
+++ lib/controller/live_search.rb
@@ -1,5 +1,5 @@
 class LiveSearchController < Ramaze::Controller
-  map '/live_search'
+  map '/jquery/live_search', :jquery
   helper :script, :stylesheet
   engine :Haml
   script 'jquery.js','jquery.liveSearch.js','css.js'
